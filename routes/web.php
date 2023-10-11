7ba17321c7af245d4b6cb5307f323c712bbbabd0
Marcos Levi <levi13.marcos@gmail.com>
2023-10-11 20:42:03 -0300

    c:18

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,10 @@ use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/events/{id}', [EventController::class, 'show']);
 
 // Rota criada para inserção de dados. Convenção do Laravel
 Route::post('/events', [EventController::class, 'store']);
+
 
 Route::get('events/contact', [EventController::class, 'contact']);
